132d2dc165d9a42ec0a2f2f493defa8ee9888585
wunschtacho <wunschtacho@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-07 09:26:46 +0000

    Scheduler integration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@43191 735d13b6-9817-0410-8766-e36946ffe9aa

--- cli/class.tx_powermail_scheduler_addFields.php
+++ cli/class.tx_powermail_scheduler_addFields.php
@@ -31,41 +31,88 @@
  */
 class tx_powermail_scheduler_addFields implements tx_scheduler_AdditionalFieldProvider {
    
-	
 	/**
-	* Function executed from the Scheduler.
+	* Add additional fields to the scheduler
 	*
 	* @return    bool
 	*/
     public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $parentObject) {
-    
+    	$additionalFields = array(); 
+		
         if (empty($taskInfo['pid'])) {
-            if($parentObject->CMD == 'edit') {
+            if ($parentObject->CMD == 'edit') {
                 $taskInfo['pid'] = $task->pid;
             } else {
                 $taskInfo['pid'] = '';
             }
         }
+		
+        if (empty($taskInfo['email'])) {
+            if ($parentObject->CMD == 'edit') {
+                $taskInfo['email'] = $task->email;
+            } else {
+                $taskInfo['email'] = '';
+            }
+        }
+		
+        if (empty($taskInfo['timeframe'])) {
+            if ($parentObject->CMD == 'edit') {
+                $taskInfo['timeframe'] = $task->timeframe;
+            } else {
+                $taskInfo['timeframe'] = '';
+            }
+        }
 
-        // Write the code for the field
+        // Write the code for the pid field
         $fieldID = 'task_pid';
-        $fieldCode = '<input type="text" name="tx_scheduler[pid]" id="' . $fieldID . '" value="' . $taskInfo['pid'] . '" size="30" />';
-        $additionalFields = array();
+        $fieldCode = '<input type="text" name="tx_scheduler[pid]" id="' . $fieldID . '" value="' . $taskInfo['pid'] . '" />';
         $additionalFields[$fieldID] = array(
             'code'     => $fieldCode,
             'label'    => 'Page ID with powermails'
         );
 
+        // Write the code for the email field
+        $fieldID = 'task_email';
+        $fieldCode = '<input type="text" name="tx_scheduler[email]" id="' . $fieldID . '" value="' . $taskInfo['email'] . '" />';
+        $additionalFields[$fieldID] = array(
+            'code'     => $fieldCode,
+            'label'    => 'Email receiver'
+        );
+
+        // Write the code for the timeframe field
+        $fieldID = 'task_timeframe';
+        $fieldCode = '<input type="text" name="tx_scheduler[timeframe]" id="' . $fieldID . '" value="' . ($taskInfo['timeframe'] == '' ? '86400' : $taskInfo['timeframe']) . '" />';
+        $additionalFields[$fieldID] = array(
+            'code'     => $fieldCode,
+            'label'    => 'Timeframe in seconds'
+        );
+
         return $additionalFields;
     }
 
+	/**
+	* Validate user values
+	*
+	* @return    bool
+	*/
     public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $parentObject) {
-        $submittedData['pid'] = intval($submittedData['pid']);
+        $submittedData['pid'] = intval($submittedData['pid']); // should be integer
+        $submittedData['timeframe'] = intval($submittedData['timeframe']); // should be integer
+        if (!t3lib_div::validEmail($submittedData['email'])) { // should be a valid email address
+			$submittedData['email'] = ''; // clean
+		}
         return true;
     }
-
+   
+	/**
+	* make values available in scheduler object
+	*
+	* @return    void
+	*/
     public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
         $task->pid = $submittedData['pid'];
+        $task->email = $submittedData['email'];
+        $task->timeframe = $submittedData['timeframe'];
     }
 }
 
