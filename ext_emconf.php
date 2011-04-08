e879ac12278d45ebb54906b8b358f2f5906c20cc
rabe69 <rabe69@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-08 06:57:38 +0000

    Changed minimum required version of typo3 to 4.4.6 because of missing ExtJs below this version, which is needed by new backend module.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@46347 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.4.0-0.0.0',
+			'typo3' => '4.4.6-0.0.0',
 		),
 		'conflicts' => array(
 		),
