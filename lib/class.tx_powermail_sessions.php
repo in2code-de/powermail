59fc2b47780aa47773007839151f087bd8a2c513
rabe69 <rabe69@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-18 16:02:31 +0000

    Fix for Bug #26866 - make powermail DBAL compatible
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@47885 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_powermail_sessions.php
+++ lib/class.tx_powermail_sessions.php
@@ -166,27 +166,28 @@ class tx_powermail_sessions extends tslib_pibase {
 			if (strlen($this->uids) > 0) $this->uids = substr($this->uids, 0, -1); // delete last ,
 		}
 		if (trim($this->uids) != '') {
-			$queryParts = array(
-				'SELECT' => 'tx_powermail_fields.uid',
-				'FROM' => '
-					tx_powermail_fields
-					INNER JOIN tx_powermail_fieldsets
-					ON
-						tx_powermail_fields.fieldset = tx_powermail_fieldsets.uid
-						' . tslib_cObj::enableFields('tx_powermail_fieldsets') . '
-					INNER JOIN tt_content
-					ON
-						tx_powermail_fieldsets.tt_content = tt_content.uid
-						AND tt_content = ' . intval($contentElementData['uid'])
-						. tslib_cObj::enableFields('tt_content'),
-				'WHERE' => '
-					tx_powermail_fields.uid IN (' . $GLOBALS['TYPO3_DB']->cleanIntList($this->uids) . ') 
-					AND (
-						tx_powermail_fields.formtype = "file" 
-						OR tx_powermail_fields.formtype="multiupload"
-					)' . tslib_cObj::enableFields('tx_powermail_fields')
-			);
-			$res = $GLOBALS['TYPO3_DB']->exec_SELECT_queryArray($queryParts);
+
+			$query = 'SELECT tx_powermail_fields.uid FROM tx_powermail_fields
+						INNER JOIN tx_powermail_fieldsets
+						ON
+							tx_powermail_fields.fieldset = tx_powermail_fieldsets.uid
+							' . tslib_cObj::enableFields('tx_powermail_fieldsets') . '
+
+						INNER JOIN tt_content
+						ON
+							tx_powermail_fieldsets.tt_content = tt_content.uid
+							AND tt_content = ' . intval($contentElementData['uid'])
+							. tslib_cObj::enableFields('tt_content') . '
+
+						WHERE
+							tx_powermail_fields.uid IN (' . $GLOBALS['TYPO3_DB']->cleanIntList($this->uids) . ')
+							AND (
+								tx_powermail_fields.formtype = "file"
+								OR tx_powermail_fields.formtype="multiupload"
+							)' . tslib_cObj::enableFields('tx_powermail_fields');
+
+			$res = $GLOBALS['TYPO3_DB']->sql_query($query);
+
 			if ($res) { // If there is a result
 				while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) { // One loop for every uploadfield
 					if ($_FILES['tx_powermail_pi1']['name']['uid' . $row['uid']]) { // if there is a content in current upload field
