13e74d21d5907008a7a351b754ca4743c15f558d
wunschtacho <wunschtacho@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-06 18:22:03 +0000

    Pi2: Working Checkin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@60445 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Condition/OrViewHelper.php
+++ Classes/ViewHelpers/Condition/OrViewHelper.php
@@ -6,11 +6,15 @@ class Tx_Powermail_ViewHelpers_Condition_OrViewHelper extends Tx_Fluid_Core_View
      * OR viewhelper for if widget in fluid
      *
      * @param 	array		Array with strings
+     * @param 	string		String to compare (if empty, just check array if there are values)
      * @return 	boolean		true/false
      */
-    public function render($array) {
+    public function render($array, $string = null) {
 		foreach ((array) $array as $value) {
-			if (!$value) {
+			if (!$string && $value) {
+				return true;
+			}
+			if ($string && $value == $string) {
 				return true;
 			}
 		}
