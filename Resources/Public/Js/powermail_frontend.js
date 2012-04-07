a9e95fe92b365b76b42ec18fc9db6441253ce87a
wunschtacho <wunschtacho@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-07 14:22:55 +0000

    Pi2: Working Checkin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@60484 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/Js/powermail_frontend.js
+++ Resources/Public/Js/powermail_frontend.js
@@ -2,14 +2,5 @@
  * Powermail_Frontend main JavaScript
  */
 jQuery(document).ready(function($) {
-	// Submit Export Form
-	$('.export_icon_xls, .export_icon_csv').click(function() {
-		if ($(this).hasClass('export_icon_csv')) {
-			$('#export_format').val('csv');
-		}
-		if ($(this).hasClass('export_icon_xls')) {
-			$('#export_format').val('xls');
-		}
-		$('#powermail_frontend_export').submit();
-	});
+
 });
