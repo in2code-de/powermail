a35918883dfcc6b79e2c343d0b930e9b13ebca99
wunschtacho <wunschtacho@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-11 21:19:30 +0000

    Spamshield integration working checkin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@60566 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Validator/SpamShieldValidator.php
+++ Classes/Domain/Validator/SpamShieldValidator.php
@@ -16,6 +16,13 @@ class Tx_Powermail_Domain_Validator_SpamShieldValidator extends Tx_Extbase_Valid
 	private $settings;
 
 	/**
+	 * Instance for Misc Functions
+	 *
+	 * @var object
+	 */
+	protected $div;
+
+	/**
 	 * Spam-Validation of given Params
 	 * 		see powermail/doc/SpamDetection for formula
 	 *
@@ -23,10 +30,14 @@ class Tx_Powermail_Domain_Validator_SpamShieldValidator extends Tx_Extbase_Valid
 	 * @return bool
 	 */
 	public function isValid($params) {
-//		t3lib_utility_Debug::debug($params, __FILE__ . " " . __LINE__);
+		t3lib_utility_Debug::debug($params, __FILE__ . " " . __LINE__);
+		$this->div = t3lib_div::makeInstance('Tx_Powermail_Utility_Div');
 		$spamFactor = $this->settings['spamshield.']['factor'] / 100;
 
 		// Different checks to increase spam indicator
+		$this->linkCheck($params, $this->settings['spamshield.']['indicator.']['name'], $this->settings['spamshield.']['indicator.']['linkLimit']);
+		$this->nameCheck($params, $this->settings['spamshield.']['indicator.']['name']);
+		$this->sessionCheck($this->settings['spamshield.']['indicator.']['session']);
 		$this->uniqueCheck($params, $this->settings['spamshield.']['indicator.']['unique']);
 		$this->blacklistStringCheck($params, $this->settings['spamshield.']['indicator.']['blacklistString']);
 		$this->blacklistIpCheck($this->settings['spamshield.']['indicator.']['blacklistIp']);
@@ -46,6 +57,107 @@ class Tx_Powermail_Domain_Validator_SpamShieldValidator extends Tx_Extbase_Valid
 	}
 
 	/**
+	 * Link Check: Counts numbers of links in message
+	 *
+	 * @param $params array Given params
+	 * @param $indication float Indication if check fails
+	 * @param $limit integer Limit of allowed links in mail
+	 * @return void
+	 */
+	private function linkCheck($params, $indication = 1, $limit = 2) {
+		if (!$indication) {
+			return;
+		}
+
+		// check numbers of links
+		$linkAmount = 0;
+		foreach ((array) $params as $value) {
+			if (is_array($value)) {
+				continue;
+			}
+			preg_match_all('@http://|https://|ftp.@', $value, $result);
+			if (isset($result[0])) {
+				$linkAmount += count($result[0]); // add numbers of http://
+			}
+		}
+
+		// if check failes
+		if ($linkAmount > $limit) {
+			$this->spamIndicator += $indication;
+		}
+		return;
+	}
+
+	/**
+	 * Name Check: Compares first- and lastname (shouldn't be the same)
+	 *
+	 * @param $params array Given params
+	 * @param $indication float Indication if check fails
+	 * @return void
+	 */
+	private function nameCheck($params, $indication = 1) {
+		if (!$indication) {
+			return;
+		}
+		$keys_firstname = array(
+			'first_name',
+			'firstname',
+			'vorname'
+		);
+		$keys_lastname = array(
+			'last_name',
+			'lastname',
+			'sur_name',
+			'surname',
+			'nachname',
+			'name'
+		);
+
+		// find out first- and lastname (marker should be {firstname} and {lastname}
+		foreach ((array) $params as $field => $value) {
+			if (is_array($value)) {
+				continue;
+			}
+			$marker = $this->div->getMarkerFromField($field);
+			if (in_array($marker, $keys_firstname)) {
+				$firstname = $value;
+			}
+			if (in_array($marker, $keys_lastname)) {
+				$lastname = $value;
+			}
+		}
+
+		// if check failes
+		if (isset($firstname) && isset($lastname)) {
+			if ($firstname && $firstname == $lastname) {
+				$this->spamIndicator += $indication;
+				return;
+			}
+		}
+	}
+
+	/**
+	 * Session Check: Checks if session was started correct on form delivery
+	 *
+	 * @param $indication float Indication if check fails
+	 * @return void
+	 */
+	private function sessionCheck($indication = 1) {
+		if (!$indication) {
+			return;
+		}
+		$gp = t3lib_div::_GP('tx_powermail_pi1');
+		$formUid = $gp['form'];
+		$time = $this->div->getFormStartFromSession($formUid);
+
+		// if check failes
+		if (!isset($time) || !$time) {
+			$this->spamIndicator += $indication;
+		}
+		return;
+	}
+
+	/**
 	 * Unique Check: Checks if values in given params are different
 	 *
 	 * @param $params array Given params
@@ -60,9 +172,10 @@ class Tx_Powermail_Domain_Validator_SpamShieldValidator extends Tx_Extbase_Valid
 		// don't want values in second level (from checkboxes e.g.)
 		$arr = array();
 		foreach ((array) $params as $value) {
-			if (!is_array($value)) {
-				$arr[] = $value;
+			if (is_array($value)) {
+				continue;
 			}
+			$arr[] = $value;
 		}
 
 		// if check failes
@@ -88,11 +201,12 @@ class Tx_Powermail_Domain_Validator_SpamShieldValidator extends Tx_Extbase_Valid
 		// if check failes
 		foreach ((array) $params as $value) {
 			foreach ((array) $blacklist as $blackword) {
-				if (!is_array($value)) {
-					if (stristr($value, $blackword)) {
-						$this->spamIndicator += $indication;
-						return;
-					}
+				if (is_array($value)) {
+					continue;
+				}
+				if (stristr($value, $blackword)) {
+					$this->spamIndicator += $indication;
+					return;
 				}
 			}
 		}
