a6a0e8b70c74b7199cbb1ec93c1cc50794889bc8
wunschtacho <wunschtacho@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-11 20:53:49 +0000

    Initial spamshield integration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/trunk@60565 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Validator/SpamShieldValidator.php
+++ Classes/Domain/Validator/SpamShieldValidator.php
@@ -2,43 +2,129 @@
 class Tx_Powermail_Domain_Validator_SpamShieldValidator extends Tx_Extbase_Validation_Validator_AbstractValidator {
 
 	/**
-	 * Return variable
+	 * Spam indication start value
 	 *
-	 * @var bool
+	 * @var integer
 	 */
-	private $isValid = true;
+	private $spamIndicator = 0.5;
 
 	/**
-	 * Validation of given Params
+	 * TypoScript Settings
+	 *
+	 * @var array
+	 */
+	private $settings;
+
+	/**
+	 * Spam-Validation of given Params
+	 * 		see powermail/doc/SpamDetection for formula
 	 *
 	 * @param $params
 	 * @return bool
 	 */
 	public function isValid($params) {
-		t3lib_utility_Debug::debug($params, __FILE__ . " " . __LINE__);
-		if (!$this->akismetCheck($params)) {
-			$this->addError('Akismet', 'spam');
-			$this->isValid = false;
+//		t3lib_utility_Debug::debug($params, __FILE__ . " " . __LINE__);
+		$spamFactor = $this->settings['spamshield.']['factor'] / 100;
+
+		// Different checks to increase spam indicator
+		$this->uniqueCheck($params, $this->settings['spamshield.']['indicator.']['unique']);
+		$this->blacklistStringCheck($params, $this->settings['spamshield.']['indicator.']['blacklistString']);
+		$this->blacklistIpCheck($this->settings['spamshield.']['indicator.']['blacklistIp']);
+
+		// spam formula with asymptote 1 (100%)
+		$thisSpamFactor = -1 / $this->spamIndicator + 1;
+
+		// if spam
+		if ($thisSpamFactor >= $spamFactor) {
+			$this->addError('spam_details', number_format(($thisSpamFactor * 100), 0) . '%');
+
+			// TODO send mail to admin
+			return false;
 		}
 
-		return $this->isValid;
+		return true;
 	}
 
 	/**
-	 * Akismet check
+	 * Unique Check: Checks if values in given params are different
 	 *
 	 * @param $params array Given params
-	 * @return bool
+	 * @param $indication float Indication if check fails
+	 * @return void
 	 */
-	private function akismetCheck($params) {
-		return false;
+	private function uniqueCheck($params, $indication = 1) {
+		if (!$indication) {
+			return;
+		}
+
+		// don't want values in second level (from checkboxes e.g.)
+		$arr = array();
+		foreach ((array) $params as $value) {
+			if (!is_array($value)) {
+				$arr[] = $value;
+			}
+		}
+
+		// if check failes
+		if (count($arr) != count(array_unique($arr))) {
+			$this->spamIndicator += $indication;
+			return;
+		}
 	}
 
-	// blackListCheck
-	// honeypodCheck
-	// linkCheck
-	// nameCheck
-	// sessionCheck
-	// uniqueCheck
+	/**
+	 * Blacklist String Check: Check if a blacklisted word is in given values
+	 *
+	 * @param $params array Given params
+	 * @param $indication float Indication if check fails
+	 * @return void
+	 */
+	private function blacklistStringCheck($params, $indication = 1) {
+		if (!$indication) {
+			return;
+		}
+		$blacklist = t3lib_div::trimExplode(',', $this->settings['spamshield.']['indicator.']['blacklistStringValues'], 1);
+
+		// if check failes
+		foreach ((array) $params as $value) {
+			foreach ((array) $blacklist as $blackword) {
+				if (!is_array($value)) {
+					if (stristr($value, $blackword)) {
+						$this->spamIndicator += $indication;
+						return;
+					}
+				}
+			}
+		}
+		return;
+	}
+
+	/**
+	 * Blacklist IP-Address Check: Check if Senders IP is blacklisted
+	 *
+	 * @param $indication float Indication if check fails
+	 * @return void
+	 */
+	private function blacklistIpCheck($indication = 1) {
+		if (!$indication) {
+			return;
+		}
+		$blacklist = t3lib_div::trimExplode(',', $this->settings['spamshield.']['indicator.']['blacklistIpValues'], 1);
+
+		// if check failes
+		if (in_array(t3lib_div::getIndpEnv('REMOTE_ADDR'), $blacklist)) {
+			$this->spamIndicator += $indication;
+			return;
+		}
+	}
+
+	/**
+	 * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
+	 * @return void
+	 */
+	public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager) {
+		$typoScriptSetup = $configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+		$this->settings = $typoScriptSetup['plugin.']['tx_powermail.']['settings.']['setup.'];
+	}
 }
 ?>
