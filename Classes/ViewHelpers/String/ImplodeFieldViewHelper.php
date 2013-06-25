d2fffb9c6257e14063c578d6b352b0eaf273374f
Alex Kellner <alexander.kellner@in2code.de>
2013-06-25 22:07:27 +0200

    #46098 Core: Invalid argument supplied for foreach() / Missing argument "1-3§ for tslib_fe::__construct()

--- Classes/ViewHelpers/String/ImplodeFieldViewHelper.php
+++ Classes/ViewHelpers/String/ImplodeFieldViewHelper.php
@@ -8,15 +8,15 @@
  */
 class Tx_Powermail_ViewHelpers_String_ImplodeFieldViewHelper extends Tx_Fluid_ViewHelpers_Form_AbstractFormFieldViewHelper {
 
-    /**
-     * View helper to explode a list
-     *
-     * @param objects $objects Any objects with submethod getUid()
-     * @param string $field Field to use in object
-     * @param string $separator Separator sign (e.g. ",")
-     * @return string
-     */
-    public function render($objects, $field = 'uid', $separator = ',') {
+	/**
+	 * View helper to explode a list
+	 *
+	 * @param objects $objects Any objects with submethod getUid()
+	 * @param string $field Field to use in object
+	 * @param string $separator Separator sign (e.g. ",")
+	 * @return string
+	 */
+	public function render($objects, $field = 'uid', $separator = ',') {
 		$string = '';
 		if (count($objects) === 0) {
 			return $string;
@@ -29,7 +29,7 @@ class Tx_Powermail_ViewHelpers_String_ImplodeFieldViewHelper extends Tx_Fluid_Vi
 			}
 		}
 		return substr($string, 0, (-1 * strlen($separator)));
-    }
+	}
 }
 
 ?>
