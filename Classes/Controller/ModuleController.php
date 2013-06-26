68e1e7c8fb91466aad2f04f2bfdd39ca1c85c185
Alex Kellner <alexander.kellner@in2code.de>
2013-06-26 22:34:23 +0200

    Fix phpdoc in BE module for T3 6.1

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -74,8 +74,8 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Check View Backend
 	 *
-	 * @param	string		email address
-	 * @return	void
+	 * @param string $email email address
+	 * @return void
 	 */
 	public function checkBeAction($email = NULL) {
 		$this->view->assign('pid', t3lib_div::_GP('id'));
@@ -101,8 +101,8 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Reporting
 	 *
-	 * @param	string		'form' or 'marketing'
-	 * @return	void
+	 * @param string $subaction could be 'form' or 'marketing'
+	 * @return void
 	 */
 	public function reportingBeAction($subaction = NULL) {
 		if ($subaction == 'form') {
@@ -116,7 +116,7 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Reporting Form
 	 *
-	 * @return	void
+	 * @return void
 	 */
 	public function reportingFormBeAction() {
 		$mails = $this->mailsRepository->findAllInPid(t3lib_div::_GP('id'), $this->settings, $this->piVars);
@@ -135,7 +135,7 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Reporting Marketing
 	 *
-	 * @return	void
+	 * @return void
 	 */
 	public function reportingMarketingBeAction() {
 		$mails = $this->mailsRepository->findAllInPid(t3lib_div::_GP('id'), $this->settings, $this->piVars);
@@ -154,8 +154,8 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Export Action
 	 *
-	 * @param	array		export settings
-	 * @return	void
+	 * @param array $export export settings
+	 * @return void
 	 */
 	public function exportBeAction(array $export = array()) {
 		if ($export['format'] == 'xls') {
@@ -167,8 +167,8 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Export Action for XLS Files
 	 *
-	 * @param	array		export settings
-	 * @return	void
+	 * @param array $export export settings
+	 * @return void
 	 */
 	public function exportXlsBeAction(array $export = array()) {
 		$mails = $this->mailsRepository->findByUidList($export['mails'], $export['sorting']);
@@ -183,8 +183,8 @@ class Tx_Powermail_Controller_ModuleController extends Tx_Extbase_MVC_Controller
 	/**
 	 * Export Action for CSV Files
 	 *
-	 * @param	array		export settings
-	 * @return	void
+	 * @param array $export export settings
+	 * @return void
 	 */
 	public function exportCsvBeAction(array $export = array()) {
 		$mails = $this->mailsRepository->findByUidList($export['mails'], $export['sorting']);
