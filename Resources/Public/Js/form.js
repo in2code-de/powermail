d24825380e45e9dc835d10623e70bcf06ec44c31
Alex Kellner <alexander.kellner@in2code.de>
2013-06-28 14:14:15 +0200

    #48738 Reduce required javascript-files

--- Resources/Public/Js/form.js
+++ Resources/Public/Js/form.js
@@ -7,41 +7,47 @@ jQuery(document).ready(function($) {
 	var baseurl = getBaseUrl();
 
 	// Form validation
-	$('.powermail_form').validationEngine();
+	if ($.fn.validationEngine) {
+		$('.powermail_form').validationEngine();
+	}
 
 	// Tabs
-	$('.powermail_morestep').powermailTabs();
+	if ($.fn.powermailTabs) {
+		$('.powermail_morestep').powermailTabs();
+	}
 
 	// Datepicker field
-	$('.powermail_date').datepicker({
-		dateFormat: $('.container_datepicker_dateformat:first').val(),
-		dayNamesMin: [
-			$('.container_datepicker_day_so:first').val(),
-			$('.container_datepicker_day_mo:first').val(),
-			$('.container_datepicker_day_tu:first').val(),
-			$('.container_datepicker_day_we:first').val(),
-			$('.container_datepicker_day_th:first').val(),
-			$('.container_datepicker_day_fr:first').val(),
-			$('.container_datepicker_day_sa:first').val()
-		],
-		monthNames: [
-			$('.container_datepicker_month_jan:first').val(),
-			$('.container_datepicker_month_feb:first').val(),
-			$('.container_datepicker_month_mar:first').val(),
-			$('.container_datepicker_month_apr:first').val(),
-			$('.container_datepicker_month_may:first').val(),
-			$('.container_datepicker_month_jun:first').val(),
-			$('.container_datepicker_month_jul:first').val(),
-			$('.container_datepicker_month_aug:first').val(),
-			$('.container_datepicker_month_sep:first').val(),
-			$('.container_datepicker_month_oct:first').val(),
-			$('.container_datepicker_month_nov:first').val(),
-			$('.container_datepicker_month_dec:first').val()
-		],
-		nextText: '&gt;',
-		prevText: '&lt;',
-		firstDay: 1
-	});
+	if ($.fn.datepicker) {
+		$('.powermail_date').datepicker({
+			dateFormat: $('.container_datepicker_dateformat:first').val(),
+			dayNamesMin: [
+				$('.container_datepicker_day_so:first').val(),
+				$('.container_datepicker_day_mo:first').val(),
+				$('.container_datepicker_day_tu:first').val(),
+				$('.container_datepicker_day_we:first').val(),
+				$('.container_datepicker_day_th:first').val(),
+				$('.container_datepicker_day_fr:first').val(),
+				$('.container_datepicker_day_sa:first').val()
+			],
+			monthNames: [
+				$('.container_datepicker_month_jan:first').val(),
+				$('.container_datepicker_month_feb:first').val(),
+				$('.container_datepicker_month_mar:first').val(),
+				$('.container_datepicker_month_apr:first').val(),
+				$('.container_datepicker_month_may:first').val(),
+				$('.container_datepicker_month_jun:first').val(),
+				$('.container_datepicker_month_jul:first').val(),
+				$('.container_datepicker_month_aug:first').val(),
+				$('.container_datepicker_month_sep:first').val(),
+				$('.container_datepicker_month_oct:first').val(),
+				$('.container_datepicker_month_nov:first').val(),
+				$('.container_datepicker_month_dec:first').val()
+			],
+			nextText: '&gt;',
+			prevText: '&lt;',
+			firstDay: 1
+		});
+	}
 
 	// Location field
 	if ($('.powermail_fieldwrap_location input').length > 0) {
