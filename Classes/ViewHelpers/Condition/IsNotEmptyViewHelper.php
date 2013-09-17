21338b918f67a3732eaed8f6438a035239dce193
Alex Kellner <alexander.kellner@in2code.de>
2013-09-17 21:35:38 +0200

    Feature #48208 option 'submit only fields with non-empty value'

---
+++ Classes/ViewHelpers/Condition/IsNotEmptyViewHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * View helper check if given value is empty (also empty arrays)
+ *
+ * @package TYPO3
+ * @subpackage Fluid
+ */
+class Tx_Powermail_ViewHelpers_Condition_IsNotEmptyViewHelper extends Tx_Fluid_ViewHelpers_Form_AbstractFormFieldViewHelper {
+
+	/**
+	 * View helper check if given value is number or not
+	 *
+	 * @param mixed $val String or Number
+	 * @return boolean
+	 */
+	public function render($val) {
+		if (!is_array($val)) {
+			return !empty($val);
+		} else {
+			foreach ((array) $val as $key => $subValue) {
+				if (!empty($subValue)) {
+					return TRUE;
+				}
+			}
+			return FALSE;
+		}
+	}
+}
+
+?>
