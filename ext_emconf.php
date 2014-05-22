cf569408d78d9310ca9890e52955c792cc75afe0
Alex Kellner <alexander.kellner@in2code.de>
2014-05-22 13:25:35 +0200

    2.0.14 Manual and Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.0.13',
+	'version' => '2.0.14',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
