5f6cfae451d59299a58538d5ca512c90ffbe1d7d
Alex Kellner <alexander.kellner@in2code.de>
2014-09-17 19:50:30 +0200

    [BUGFIX] addFieldOptions fails in IRRE if set in page properties
    related: #57080

--- Classes/Utility/FlexFormFieldSelection.php
+++ Classes/Utility/FlexFormFieldSelection.php
@@ -24,10 +24,10 @@ class Tx_Powermail_Utility_FlexFormFieldSelection {
 	 * @return void
 	 */
 	public function addOptions(&$params, &$pObj) {
-		$TSConfig = t3lib_BEfunc::getPagesTSconfig($this->getPid());
+		$tSConfig = t3lib_BEfunc::getPagesTSconfig($params['row']['pid']);
 
-		if (!empty($TSConfig[$this->extension . '.']['flexForm.'][$params['config']['itemsProcFuncFieldName'] . '.']['addFieldOptions.'])) {
-			$options = $TSConfig[$this->extension . '.']['flexForm.'][$params['config']['itemsProcFuncFieldName'] . '.']['addFieldOptions.'];
+		if (!empty($tSConfig[$this->extension . '.']['flexForm.'][$params['config']['itemsProcFuncFieldName'] . '.']['addFieldOptions.'])) {
+			$options = $tSConfig[$this->extension . '.']['flexForm.'][$params['config']['itemsProcFuncFieldName'] . '.']['addFieldOptions.'];
 			foreach ((array) $options as $value => $label) {
 				$params['items'][] = array(
 					$label,
@@ -36,25 +36,4 @@ class Tx_Powermail_Utility_FlexFormFieldSelection {
 			}
 		}
 	}
-
-	/**
-	 * Read pid from current URL
-	 * 		URL example: http://powermailt361.in2code.de/typo3/alt_doc.php?&returnUrl=%2Ftypo3%2Fsysext%2Fcms%2Flayout%2Fdb_layout.php%3Fid%3D17%23element-tt_content-14&edit[tt_content][14]=edit
-	 *
-	 * @return int
-	 */
-	protected function getPid() {
-		$pid = 0;
-		$backUrl = str_replace('?', '&', t3lib_div::_GP('returnUrl'));
-		$urlParts = t3lib_div::trimExplode('&', $backUrl, 1);
-		foreach ($urlParts as $part) {
-			if (stristr($part, 'id=')) {
-				$pid = str_replace('id=', '', $part);
-			}
-		}
-
-		return intval($pid);
-	}
-
 }
-?>
