6e60ae2571c1d851d72a4bd0021b508f86180afd
Alex Kellner <alexander.kellner@in2code.de>
2014-11-26 10:55:28 +0100

    [TASK] Added Unit test for powermail version note
    see description + refactored PowermailVersionNoteViewHelper.php

--- Classes/ViewHelpers/BeCheck/PowermailVersionNoteViewHelper.php
+++ Classes/ViewHelpers/BeCheck/PowermailVersionNoteViewHelper.php
@@ -25,20 +25,63 @@ class PowermailVersionNoteViewHelper extends AbstractViewHelper {
 	protected $status = 0;
 
 	/**
+	 * Current powermail version
+	 *
+	 * @var string
+	 */
+	protected $version = NULL;
+
+	/**
+	 * Extension Manager table with all extensions exist
+	 *
+	 * @var bool
+	 */
+	protected $extensionTableExists = FALSE;
+
+	/**
+	 * Is there a newer version of powermail
+	 *
+	 * @var bool
+	 */
+	protected $isNewerVersionAvailable = FALSE;
+
+	/**
+	 * This version is listed in EM table
+	 *
+	 * @var bool
+	 */
+	protected $currentVersionInExtensionTableExists = FALSE;
+
+	/**
+	 * This version is unsecure
+	 *
+	 * @var bool
+	 */
+	protected $isCurrentVersionUnsecure = FALSE;
+
+	/**
+	 * Check in database (disable for testing only)
+	 *
+	 * @var bool
+	 */
+	protected $checkFromDatabase = TRUE;
+
+	/**
 	 * Return powermail version
 	 *
 	 * @return int
 	 */
 	public function render() {
-		if (!$this->extensionTableExists()) {
+		$this->init();
+		if (!$this->getExtensionTableExists()) {
 			return $this->getStatus();
 		}
-		if ($this->isVersionInTable()) {
+		if ($this->getCurrentVersionInExtensionTableExists()) {
 			$this->setStatus(1);
-			if ($this->isNewerVersionAvailable()) {
+			if ($this->getIsNewerVersionAvailable()) {
 				$this->setStatus(3);
 			}
-			if (!$this->isCurrentVersionSecure()) {
+			if ($this->getIsCurrentVersionUnsecure()) {
 				$this->setStatus(2);
 			}
 		}
@@ -46,9 +89,42 @@ class PowermailVersionNoteViewHelper extends AbstractViewHelper {
 	}
 
 	/**
+	 * Database Preflight
+	 * @return void
+	 */
+	protected function init() {
+		if (!$this->getVersion()) {
+			$this->setVersion(Div::getVersion());
+		}
+		if ($this->getCheckFromDatabase()) {
+			$this->setIsCurrentVersionUnsecure($this->getIsCurrentVersionUnsecureFromDatabase());
+			$this->setIsNewerVersionAvailable($this->getIsNewerVersionAvailableFromDatabase());
+			$this->setExtensionTableExists($this->getExtensionTableExistsFromDatabase());
+			$this->setCurrentVersionInExtensionTableExists($this->getCurrentVersionInExtensionTableExistsFromDatabase());
+		}
+	}
+
+	/**
 	 * @return bool
 	 */
-	protected function isNewerVersionAvailable() {
+	protected function getIsCurrentVersionUnsecureFromDatabase() {
+		$select = 'review_state';
+		$from = 'tx_extensionmanager_domain_model_extension';
+		$where = 'extension_key = "powermail" and version = "' . $this->getVersion() . '"';
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $from, $where, '', '', 1);
+		if ($res) {
+			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+			if ($row['review_state'] === '0') {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function getIsNewerVersionAvailableFromDatabase() {
 		$select = 'version';
 		$from = 'tx_extensionmanager_domain_model_extension';
 		$where = 'extension_key = "powermail"';
@@ -57,7 +133,7 @@ class PowermailVersionNoteViewHelper extends AbstractViewHelper {
 			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
 			if (!empty($row['version'])) {
 				$newestVersion = VersionNumberUtility::convertVersionNumberToInteger($row['version']);
-				$currentVersion = VersionNumberUtility::convertVersionNumberToInteger(Div::getVersion());
+				$currentVersion = VersionNumberUtility::convertVersionNumberToInteger($this->getVersion());
 				if ($currentVersion < $newestVersion) {
 					return TRUE;
 				}
@@ -69,44 +145,27 @@ class PowermailVersionNoteViewHelper extends AbstractViewHelper {
 	/**
 	 * @return bool
 	 */
-	protected function isVersionInTable() {
+	protected function getExtensionTableExistsFromDatabase() {
+		$allTables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+		if (array_key_exists('tx_extensionmanager_domain_model_extension', $allTables)) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function getCurrentVersionInExtensionTableExistsFromDatabase() {
 		$select = 'uid';
 		$from = 'tx_extensionmanager_domain_model_extension';
-		$where = 'extension_key = "powermail" and version = "' . Div::getVersion() . '"';
+		$where = 'extension_key = "powermail" and version = "' . $this->getVersion() . '"';
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $from, $where, '', '', 1);
 		if ($res) {
 			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
 			if (!empty($row['uid'])) {
 				return TRUE;
 			}
-		}
-		return FALSE;
-	}
-
-	/**
-	 * @return bool
-	 */
-	protected function isCurrentVersionSecure() {
-		$select = 'review_state';
-		$from = 'tx_extensionmanager_domain_model_extension';
-		$where = 'extension_key = "powermail" and version = "' . Div::getVersion() . '"';
-		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $from, $where, '', '', 1);
-		if ($res) {
-			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
-			if ($row['review_state'] === '0') {
-				return TRUE;
-			}
-		}
-		return FALSE;
-	}
-
-	/**
-	 * @return bool
-	 */
-	protected function extensionTableExists() {
-		$allTables = $GLOBALS['TYPO3_DB']->admin_get_tables();
-		if (array_key_exists('tx_extensionmanager_domain_model_extension', $allTables)) {
-			return TRUE;
 		}
 		return FALSE;
 	}
@@ -124,5 +183,95 @@ class PowermailVersionNoteViewHelper extends AbstractViewHelper {
 	 */
 	public function setStatus($status) {
 		$this->status = $status;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getVersion() {
+		return $this->version;
+	}
+
+	/**
+	 * @param string $version
+	 * @return void
+	 */
+	public function setVersion($version) {
+		$this->version = $version;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getCheckFromDatabase() {
+		return $this->checkFromDatabase;
+	}
+
+	/**
+	 * @param bool $checkFromDatabase
+	 * @return void
+	 */
+	public function setCheckFromDatabase($checkFromDatabase) {
+		$this->checkFromDatabase = $checkFromDatabase;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getExtensionTableExists() {
+		return $this->extensionTableExists;
+	}
+
+	/**
+	 * @param bool $extensionTableExists
+	 * @return void
+	 */
+	public function setExtensionTableExists($extensionTableExists) {
+		$this->extensionTableExists = $extensionTableExists;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getIsCurrentVersionUnsecure() {
+		return $this->isCurrentVersionUnsecure;
+	}
+
+	/**
+	 * @param bool $isCurrentVersionUnsecure
+	 * @return void
+	 */
+	public function setIsCurrentVersionUnsecure($isCurrentVersionUnsecure) {
+		$this->isCurrentVersionUnsecure = $isCurrentVersionUnsecure;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getIsNewerVersionAvailable() {
+		return $this->isNewerVersionAvailable;
+	}
+
+	/**
+	 * @param bool $isNewerVersionAvailable
+	 * @return void
+	 */
+	public function setIsNewerVersionAvailable($isNewerVersionAvailable) {
+		$this->isNewerVersionAvailable = $isNewerVersionAvailable;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getCurrentVersionInExtensionTableExists() {
+		return $this->currentVersionInExtensionTableExists;
+	}
+
+	/**
+	 * @param bool $currentVersionInExtensionTableExists
+	 * @return void
+	 */
+	public function setCurrentVersionInExtensionTableExists($currentVersionInExtensionTableExists) {
+		$this->currentVersionInExtensionTableExists = $currentVersionInExtensionTableExists;
 	}
 }
---
+++ Tests/Unit/ViewHelpers/BeCheck/PowermailVersionNoteViewHelperTest.php
@@ -0,0 +1,141 @@
+<?php
+namespace In2code\Powermail\Tests\ViewHelpers\BeCheck;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Alex Kellner <alexander.kellner@in2code.de>, in2code.de
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * PowermailVersionNoteViewHelper Test
+ *
+ * @package powermail
+ * @license http://www.gnu.org/licenses/lgpl.html
+ * 			GNU Lesser General Public License, version 3 or later
+ */
+class PowermailVersionNoteViewHelperTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
+
+	/**
+	 * @var \TYPO3\CMS\Core\Tests\AccessibleObjectInterface
+	 */
+	protected $abstractValidationViewHelperMock;
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		$this->abstractValidationViewHelperMock = $this->getAccessibleMock(
+			'\In2code\Powermail\ViewHelpers\BeCheck\PowermailVersionNoteViewHelper',
+			array('dummy')
+		);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function tearDown() {
+		unset($this->generalValidatorMock);
+	}
+
+	/**
+	 * Dataprovider for renderReturnsInt()
+	 *
+	 * @return array
+	 */
+	public function renderReturnsIntDataProvider() {
+		return array(
+			array(
+				FALSE,
+				FALSE,
+				FALSE,
+				FALSE,
+				0
+			),
+			array(
+				TRUE,
+				TRUE,
+				TRUE,
+				FALSE,
+				3
+			),
+			array(
+				FALSE,
+				TRUE,
+				TRUE,
+				TRUE,
+				0
+			),
+			array(
+				TRUE,
+				FALSE,
+				TRUE,
+				FALSE,
+				1
+			),
+			array(
+				TRUE,
+				TRUE,
+				TRUE,
+				TRUE,
+				2
+			),
+			array(
+				TRUE,
+				FALSE,
+				TRUE,
+				TRUE,
+				2
+			),
+		);
+	}
+
+	/**
+	 * Test for render()
+	 *
+	 * @param bool $extensionTableExists
+	 * @param bool $isNewerVersionAvailable
+	 * @param bool $currentVersionInExtensionTableExists
+	 * @param bool $isCurrentVersionUnsecure
+	 * @param int $expectedResult
+	 * @return void
+	 * @dataProvider renderReturnsIntDataProvider
+	 * @test
+	 */
+	public function renderReturnsInt(
+		$extensionTableExists,
+		$isNewerVersionAvailable,
+		$currentVersionInExtensionTableExists,
+		$isCurrentVersionUnsecure,
+		$expectedResult
+	) {
+		$this->abstractValidationViewHelperMock->_set('checkFromDatabase', FALSE);
+		$this->abstractValidationViewHelperMock->_callRef('setExtensionTableExists', $extensionTableExists);
+		$this->abstractValidationViewHelperMock->_callRef('setIsNewerVersionAvailable', $isNewerVersionAvailable);
+		$this->abstractValidationViewHelperMock->_callRef(
+			'setCurrentVersionInExtensionTableExists',
+			$currentVersionInExtensionTableExists
+		);
+		$this->abstractValidationViewHelperMock->_callRef('setIsCurrentVersionUnsecure', $isCurrentVersionUnsecure);
+		$result = $this->abstractValidationViewHelperMock->_callRef('render');
+		$this->assertSame($expectedResult, $result);
+	}
+}
