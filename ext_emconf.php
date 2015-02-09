8218ab99d9c8d471aa01e5176bfc4ddc8c892f3c
Alex Kellner <alexander.kellner@in2code.de>
2015-02-09 20:14:22 +0100

    [TASK] Version 2.0.20

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.0.19',
+	'version' => '2.0.20',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
