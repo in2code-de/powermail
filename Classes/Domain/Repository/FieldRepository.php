56e66201d6b3adcf5885758aef0a17f4dacd995e
Alex Kellner <alexander.kellner@in2code.de>
2015-02-26 13:36:26 +0100

    [BUGFIX] No empty values in result of FieldRepository->findByUid()
    The FieldRepository->findByUid() method does not contain NULL values
    in the returned array any more. Only if a field record is found for
    a given UID it will be added to the return array.
    
    Change-Id: Ic413f813a097155a2fcb55a5b4bf85d5210d9f15
    Related: #65263

--- Classes/Domain/Repository/FieldRepository.php
+++ Classes/Domain/Repository/FieldRepository.php
@@ -1,6 +1,8 @@
 <?php
 namespace In2code\Powermail\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
 /***************************************************************
  *  Copyright notice
  *
@@ -24,7 +26,6 @@ namespace In2code\Powermail\Domain\Repository;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-
 /**
  * FieldRepository
  *
@@ -32,7 +33,7 @@ namespace In2code\Powermail\Domain\Repository;
  * @license http://www.gnu.org/licenses/lgpl.html
  * 			GNU Lesser General Public License, version 3 or later
  */
-class FieldRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+class FieldRepository extends Repository {
 
 	/**
 	 * formRepository
@@ -54,7 +55,10 @@ class FieldRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 		foreach ($uids as $uid) {
 			$query = $this->createQuery();
 			$query->getQuerySettings()->setRespectStoragePage(FALSE);
-			$result[] = $query->matching($query->equals('uid', $uid))->execute()->getFirst();
+			$field = $query->matching($query->equals('uid', $uid))->execute()->getFirst();
+			if ($field !== NULL) {
+				$result[] = $field;
+			}
 		}
 		return $result;
 	}
