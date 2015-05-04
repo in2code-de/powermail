39344aad199983d275990ffdff1f393b90f8db8d
Alex Kellner <alexander.kellner@in2code.de>
2015-05-04 17:23:07 +0200

    [BUGFIX] No identifying value for object of class "" found.
    refactored SelectFieldViewHelper.php for TYPO3 7.2
    
    related: #66634

--- Classes/ViewHelpers/Form/SelectFieldViewHelper.php
+++ Classes/ViewHelpers/Form/SelectFieldViewHelper.php
@@ -12,39 +12,75 @@ use TYPO3\CMS\Fluid\ViewHelpers\Form\SelectViewHelper;
 class SelectFieldViewHelper extends SelectViewHelper {
 
 	/**
+	 * @var array
+	 */
+	protected $originalOptions = array();
+
+	/**
 	 * Render the tag.
 	 *
 	 * @return string rendered tag.
 	 * @api
 	 */
 	public function render() {
-		$content = parent::render();
-
-		return $content;
+		$this->originalOptions = $this->arguments['options'];
+		$this->setOptions();
+		return parent::render();
 	}
 
 	/**
-	 * Render the option tags.
+	 * Set options with key and value from $field->getModifiedOptions()
+	 * 		convert:
+	 * 			array(
+	 * 				array(
+	 * 					'label' => 'Red shoes',
+	 * 					'value' => 'red',
+	 * 					'selected' => 0
+	 * 				)
+	 * 			)
 	 *
-	 * @param array $options the options for the form.
-	 * @return string rendered tags.
+	 * 		to:
+	 * 			array(
+	 * 				'red' => 'Red shoes'
+	 * 			)
+	 *
+	 *
+	 * @return void
 	 */
-	protected function renderOptionTags($options) {
-		$output = '';
-		if ($this->hasArgument('prependOptionLabel')) {
-			$value = $this->hasArgument('prependOptionValue') ? $this->arguments['prependOptionValue'] : '';
-			$label = $this->arguments['prependOptionLabel'];
-			$output .= $this->renderOptionTag($value, $label, FALSE) . chr(10);
+	protected function setOptions() {
+		$optionArray = array();
+		foreach ($this->arguments['options'] as $option) {
+			$optionArray[$option['value']] = $option['label'];
 		}
-		foreach ($options as $option) {
-			$output .= $this->renderOptionTag(
-				$option['value'],
-				$option['label'],
-				$this->isSelectedAlternative($option)
-			);
-			$output .= chr(10);
+		$this->arguments['options'] = $optionArray;
+	}
+
+	/**
+	 * Render one option tag
+	 *
+	 * @param string $value value attribute of the option tag (will be escaped)
+	 * @param string $label content of the option tag (will be escaped)
+	 * @return string the rendered option tag
+	 */
+	protected function renderOptionTag($value, $label) {
+		return parent::renderOptionTag(
+			$value,
+			$label,
+			$this->isSelectedAlternative($this->getOptionFromOriginalOptionsByValue($value))
+		);
+	}
+
+	/**
+	 * @param string $value
+	 * @return array
+	 */
+	protected function getOptionFromOriginalOptionsByValue($value) {
+		foreach ($this->originalOptions as $option) {
+			if ($value === $option['value'] || $value === $option['label']) {
+				return $option;
+			}
 		}
-		return $output;
+		return array();
 	}
 
 	/**
@@ -54,24 +90,38 @@ class SelectFieldViewHelper extends SelectViewHelper {
 	 * @return boolean TRUE if the value marked a s selected; FALSE otherwise
 	 */
 	protected function isSelectedAlternative($option) {
-		if (!is_array($this->getValue())) {
-			if (
-				// preselect from flexform
-				($option['selected'] && !$this->getValue()) ||
-				// preselect from piVars
-				($this->getValue() && ($option['value'] === $this->getValue() || $option['label'] === $this->getValue()))
-			) {
+		if (is_array($this->getValue())) {
+			return $this->isSelectedAlternativeForArray($option);
+		}
+		return $this->isSelectedAlternativeForString($option);
+	}
+
+	/**
+	 * @param array $option
+	 * @return bool
+	 */
+	protected function isSelectedAlternativeForString($option) {
+		if (
+			// preselect from flexform
+			($option['selected'] && !$this->getValue()) ||
+			// preselect from piVars
+			($this->getValue() && ($option['value'] === $this->getValue() || $option['label'] === $this->getValue()))
+		) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @param array $option
+	 * @return bool
+	 */
+	protected function isSelectedAlternativeForArray($option) {
+		foreach ($this->getValue() as $singleValue) {
+			if (!empty($singleValue) && ($option['value'] === $singleValue || $option['label'] === $singleValue)) {
 				return TRUE;
 			}
-		} else {
-			// Multi Select
-			foreach ($this->getValue() as $singleValue) {
-				if (!empty($singleValue) && ($option['value'] === $singleValue || $option['label'] === $singleValue)) {
-					return TRUE;
-				}
-			}
 		}
-
 		return FALSE;
 	}
 }
