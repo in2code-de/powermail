1e118f4a3a9e9f8f92db29cf39a593cd1db7e6a6
Alex Kellner <alexander.kellner@in2code.de>
2015-10-02 16:08:24 +0200

    [TASK] Remove variable $variablesMarkers from \In2code\Powermail\ViewHelpers\Misc\VariablesViewHelper::render()
    removed variable from render() method
    
    related: #66996

--- Classes/ViewHelpers/Misc/VariablesViewHelper.php
+++ Classes/ViewHelpers/Misc/VariablesViewHelper.php
@@ -47,21 +47,9 @@ class VariablesViewHelper extends AbstractViewHelper {
 	 * @param \In2code\Powermail\Domain\Model\Mail $mail Variables and Labels array
 	 * @param string $type "web" or "mail"
 	 * @param string $function "createAction", "senderMail", "receiverMail"
-	 * @param array $variablesMarkers - deprecated parameter - will be removed
-	 * @todo remove param $variablesMarkers
 	 * @return string Changed string
 	 */
-	public function render(Mail $mail, $type = 'web', $function = 'createAction', $variablesMarkers = array()) {
-		// TODO remove $variablesMarkers completely in next minor version
-		if (count($variablesMarkers)) {
-			GeneralUtility::deprecationLog(
-				'Method \In2code\Powermail\ViewHelpers\Misc\VariablesViewHelper::render() was called from a ' .
-				'template or a partial with attribute "variablesMarkers". This attribute will be removed in next ' .
-				'minor version of powermail. Further use can lead to exceptions. Please remove this attribute ' .
-				'from your template files.'
-			);
-		}
-
+	public function render(Mail $mail, $type = 'web', $function = 'createAction') {
 		/** @var \TYPO3\CMS\Fluid\View\StandaloneView $parseObject */
 		$parseObject = $this->objectManager->get('TYPO3\\CMS\\Fluid\\View\\StandaloneView');
 		$parseObject->setTemplateSource($this->removePowermailAllParagraphTagWrap($this->renderChildren()));
