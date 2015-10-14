fa6e76cf18e918d8d04e2ca5c43bf14e983217d8
Alex Kellner <alexander.kellner@in2code.de>
2015-10-14 10:14:50 +0200

    [BUGFIX] Error in documentation (For editors / checkboxes)
    Documentation update
    
    related: #70656

--- Classes/Utility/Hook/AbstractMarker.php
+++ Classes/Utility/Hook/AbstractMarker.php
@@ -34,7 +34,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * @license http://www.gnu.org/licenses/lgpl.html
  * 			GNU Lesser General Public License, version 3 or later
  */
-class AbstractMarker {
+abstract class AbstractMarker {
 
 	/**
 	 * Array with all GET/POST params to save
