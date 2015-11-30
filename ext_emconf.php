c1af111dc625e9dda877b21873ede845fc99e9ad
Alex Kellner <alexander.kellner@in2code.de>
2015-11-30 11:24:42 +0100

    [TASK] Add PHP 5.5 to the minimum requirements of powermail
    Added requirements to ext_emconf.php
    
    related: #71976

--- ext_emconf.php
+++ ext_emconf.php
@@ -38,7 +38,8 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'typo3' => '6.2.7-7.99.99',
             'extbase' => '6.2.0-7.99.99',
-            'fluid' => '6.2.0-7.99.99'
+            'fluid' => '6.2.0-7.99.99',
+            'php' => '5.5.0-0.0.0'
         ),
         'conflicts' => array(),
         'suggests' => array(),
