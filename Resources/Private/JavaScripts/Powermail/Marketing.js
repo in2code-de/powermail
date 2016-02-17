71689bf6da50a39f2658d40fe297363f2270faa4
Alex Kellner <alexander.kellner@in2code.de>
2016-02-17 15:11:05 +0100

    [TASK] Use private functions and properties in JavaScript
    Rewrite private functions and properties from this. to var

--- Resources/Private/JavaScripts/Powermail/Marketing.js
+++ Resources/Private/JavaScripts/Powermail/Marketing.js
@@ -8,37 +8,31 @@ function PowermailMarketing($) {
 	'use strict';
 
 	/**
-	 * This class
-	 *
-	 * @type {PowermailMarketing}
-	 */
-	var that = this;
-
-	/**
 	 * Initialize
 	 *
 	 * @returns {void}
 	 */
 	this.initialize = function() {
-		that.sendMarketingInformation();
+		sendMarketingInformation();
 	};
 
 	/**
 	 * Send marketing information to eID script
 	 *
 	 * @returns {void}
+	 * @private
 	 */
-	this.sendMarketingInformation = function() {
+	var sendMarketingInformation = function() {
 		var $marketingInformation = $('#powermail_marketing_information');
 		var data = '';
 		data += 'tx_powermail_pi1[language]=' + $marketingInformation.data('language');
 		data += '&id=' + $marketingInformation.data('pid');
 		data += '&tx_powermail_pi1[pid]=' + $marketingInformation.data('pid');
-		data += '&tx_powermail_pi1[mobileDevice]=' + (that.isMobile() ? 1 : 0);
+		data += '&tx_powermail_pi1[mobileDevice]=' + (isMobile() ? 1 : 0);
 		data += '&tx_powermail_pi1[referer]=' + encodeURIComponent(document.referrer);
 
 		jQuery.ajax({
-			url: that.getBaseUrl() + '/index.php?&eID=powermailEidMarketing',
+			url: getBaseUrl() + '/index.php?&eID=powermailEidMarketing',
 			data: data,
 			cache: false
 		});
@@ -48,8 +42,9 @@ function PowermailMarketing($) {
 	 * Check if user device is mobile or not
 	 *
 	 * @return bool
+	 * @private
 	 */
-	this.isMobile = function() {
+	var isMobile = function() {
 		var ua = navigator.userAgent;
 		var checker = {
 			iphone:ua.match(/(iPhone|iPod|iPad)/),
@@ -63,11 +58,13 @@ function PowermailMarketing($) {
 	 * Return BaseUrl as prefix
 	 *
 	 * @return {string} Base Url
+	 * @private
 	 */
-	this.getBaseUrl = function() {
+	var getBaseUrl = function() {
 		var baseurl;
-		if ($('base').length > 0) {
-			baseurl = $('base').prop('href');
+		var $base = $('base');
+		if ($base.length > 0) {
+			baseurl = $base.prop('href');
 		} else {
 			if (window.location.protocol != "https:") {
 				baseurl = 'http://' + window.location.hostname;
