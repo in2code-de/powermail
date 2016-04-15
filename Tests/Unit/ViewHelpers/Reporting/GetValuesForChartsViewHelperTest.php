6293ac64a20453c443dc3c32e021c801e9459d23
Alex Kellner <alexander.kellner@in2code.de>
2016-04-15 18:52:43 +0200

    [TASK] Refactoring for version 3.0
    Updating unit test

--- Tests/Unit/ViewHelpers/Reporting/GetValuesForChartsViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Reporting/GetValuesForChartsViewHelperTest.php
@@ -1,38 +1,35 @@
 <?php
 namespace In2code\Powermail\Tests\ViewHelpers\Reporting;
 
-    /***************************************************************
-     *  Copyright notice
-     *
-     *  (c) 2014 Alex Kellner <alexander.kellner@in2code.de>, in2code.de
-     *
-     *  All rights reserved
-     *
-     *  This script is part of the TYPO3 project. The TYPO3 project is
-     *  free software; you can redistribute it and/or modify
-     *  it under the terms of the GNU General Public License as published by
-     *  the Free Software Foundation; either version 3 of the License, or
-     *  (at your option) any later version.
-     *
-     *  The GNU General Public License can be found at
-     *  http://www.gnu.org/copyleft/gpl.html.
-     *
-     *  This script is distributed in the hope that it will be useful,
-     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     *  GNU General Public License for more details.
-     *
-     *  This copyright notice MUST APPEAR in all copies of the script!
-     ***************************************************************/
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Alex Kellner <alexander.kellner@in2code.de>, in2code.de
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
- * GetLabelsGoogleChartsViewHelper Test
- *
- * @package powermail
- * @license http://www.gnu.org/licenses/lgpl.html
- *          GNU Lesser General Public License, version 3 or later
+ * Class GetValuesForChartsViewHelperTest
+ * @package In2code\Powermail\Tests\ViewHelpers\Reporting
  */
-class GetValuesGoogleChartsViewHelperTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
+class GetValuesForChartsViewHelperTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
 {
 
     /**
@@ -46,7 +43,7 @@ class GetValuesGoogleChartsViewHelperTest extends \TYPO3\CMS\Core\Tests\UnitTest
     public function setUp()
     {
         $this->abstractValidationViewHelperMock = $this->getAccessibleMock(
-            '\In2code\Powermail\ViewHelpers\Reporting\GetValuesGoogleChartsViewHelper',
+            '\In2code\Powermail\ViewHelpers\Reporting\GetValuesForChartsViewHelper',
             ['dummy']
         );
     }
