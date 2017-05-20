3ad0cd3307ec8c415f43d903ebf6b97e9746c50b
Alex Kellner <alexander.kellner@in2code.de>
2017-05-20 22:37:42 +0200

    [TASK] Update TCA configuration for TYPO3 8.7
    Following the instructions under install tool / TCA Migrations
    
    related: https://github.com/einpraegsam/powermail/issues/92

--- Configuration/TCA/tx_powermail_domain_model_form.php
+++ Configuration/TCA/tx_powermail_domain_model_form.php
@@ -12,8 +12,7 @@ $formsTca = [
         'crdate' => 'crdate',
         'cruser_id' => 'cruser_id',
         'dividers2tabs' => true,
-        'versioningWS' => 2,
-        'versioning_followPages' => true,
+        'versioningWS' => true,
         'origUid' => 't3_origuid',
         'languageField' => 'sys_language_uid',
         'transOrigPointerField' => 'l10n_parent',
@@ -97,9 +96,9 @@ $formsTca = [
             'label' => 'LLL:EXT:powermail/Resources/Private/Language/locallang_db.xlf:LGL.starttime',
             'config' => [
                 'type' => 'input',
-                'size' => 13,
-                'max' => 20,
+                'renderType' => 'inputDateTime',
                 'eval' => 'datetime',
+                'size' => 13,
                 'checkbox' => 0,
                 'default' => 0,
                 'range' => [
@@ -113,9 +112,9 @@ $formsTca = [
             'label' => 'LLL:EXT:powermail/Resources/Private/Language/locallang_db.xlf:LGL.endtime',
             'config' => [
                 'type' => 'input',
-                'size' => 13,
-                'max' => 20,
+                'renderType' => 'inputDateTime',
                 'eval' => 'datetime',
+                'size' => 13,
                 'checkbox' => 0,
                 'default' => 0,
                 'range' => [
@@ -196,8 +195,7 @@ $formsTca = [
                     'showRemovedLocalizationRecords' => 1,
                 ],
                 'behaviour' => [
-                    'localizeChildrenAtParentLocalization' => 1,
-                    'localizationMode' => 'select',
+                    'localizeChildrenAtParentLocalization' => 1
                 ],
             ],
         ],
