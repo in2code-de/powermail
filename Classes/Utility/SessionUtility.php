5fbc4175f0135486793fd972a75ccf80c6173cac
Alex Kellner <alexander.kellner@in2code.de>
2017-08-14 22:05:02 +0200

    [TASK] Small code cleanup

--- Classes/Utility/SessionUtility.php
+++ Classes/Utility/SessionUtility.php
@@ -8,35 +8,8 @@ use In2code\Powermail\Domain\Validator\SpamShield\SessionMethod;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Service\TypoScriptService;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2015 Alex Kellner <alexander.kellner@in2code.de>, in2code.de
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 /**
- * Session utility functions
- *
- * @package powermail
- * @license http://www.gnu.org/licenses/lgpl.html
- *          GNU Lesser General Public License, version 3 or later
+ * Class SessionUtility
  */
 class SessionUtility extends AbstractUtility
 {
@@ -174,8 +147,7 @@ class SessionUtility extends AbstractUtility
         $typoScriptService = self::getObjectManager()->get(TypoScriptService::class);
         $contentObject = self::getContentObject();
         $configuration = $typoScriptService->convertPlainArrayToTypoScriptArray($settings);
-        if (
-            !empty($configuration['saveSession.']) &&
+        if (!empty($configuration['saveSession.']) &&
             array_key_exists($configuration['saveSession.']['_method'], self::$methods)
         ) {
             $mailRepository = self::getObjectManager()->get(MailRepository::class);
@@ -211,8 +183,7 @@ class SessionUtility extends AbstractUtility
     public static function getSessionValuesForPrefill($configuration)
     {
         $values = [];
-        if (
-            !empty($configuration['saveSession.']) &&
+        if (!empty($configuration['saveSession.']) &&
             array_key_exists($configuration['saveSession.']['_method'], self::$methods)
         ) {
             $values = self::getSessionValue(
