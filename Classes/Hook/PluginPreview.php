e944809748fe43991b66d87b6058441be757ccb9
Alex Kellner <alexander.kellner@in2code.de>
2017-11-14 10:26:49 +0100

    [TASK] Small code cleanup in PluginPreview

--- Classes/Hook/PluginPreview.php
+++ Classes/Hook/PluginPreview.php
@@ -1,40 +1,17 @@
 <?php
 namespace In2code\Powermail\Hooks;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2016 Alex Kellner <alexander.kellner@in2code.de>, in2code.de
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 use In2code\Powermail\Domain\Model\Form;
 use In2code\Powermail\Domain\Repository\MailRepository;
 use In2code\Powermail\Utility\ArrayUtility;
 use In2code\Powermail\Utility\ConfigurationUtility;
 use In2code\Powermail\Utility\ObjectUtility;
 use In2code\Powermail\Utility\TemplateUtility;
+use TYPO3\CMS\Backend\Utility\BackendUtility as BackendUtilityCore;
 use TYPO3\CMS\Backend\View\PageLayoutView;
 use TYPO3\CMS\Backend\View\PageLayoutViewDrawItemHookInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Persistence\Generic\QueryResult;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Service\FlexFormService;
 
 /**
@@ -125,7 +102,7 @@ class PluginPreview implements PageLayoutViewDrawItemHookInterface
     /**
      * Get latest three emails to this form
      *
-     * @return QueryResult
+     * @return QueryResultInterface
      */
     protected function getLatestMails()
     {
@@ -164,7 +141,7 @@ class PluginPreview implements PageLayoutViewDrawItemHookInterface
     protected function getFormTitleByUid($uid)
     {
         $uid = $this->getLocalizedFormUid($uid, $this->getSysLanguageUid());
-        $row = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord(
+        $row = BackendUtilityCore::getRecord(
             Form::TABLE_NAME,
             $uid,
             'title',
@@ -184,7 +161,7 @@ class PluginPreview implements PageLayoutViewDrawItemHookInterface
     protected function getLocalizedFormUid($uid, $sysLanguageUid)
     {
         if ($sysLanguageUid > 0) {
-            $row = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecordLocalization(
+            $row = BackendUtilityCore::getRecordLocalization(
                 Form::TABLE_NAME,
                 (int)$uid,
                 (int)$sysLanguageUid
@@ -226,8 +203,6 @@ class PluginPreview implements PageLayoutViewDrawItemHookInterface
     protected function initialize(array $row)
     {
         $this->row = $row;
-
-        /** @var FlexFormService $flexFormService */
         $flexFormService = ObjectUtility::getObjectManager()->get(FlexFormService::class);
         $this->flexFormData = $flexFormService->convertFlexFormContentToArray($this->row['pi_flexform']);
     }
