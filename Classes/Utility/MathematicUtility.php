95cceab731cac9f40f50487cd90b267867043664
Alex Kellner <alexander.kellner@in2code.de>
2017-12-22 12:45:27 +0100

    [TASK] Cleanup of the CalculatingCaptchaService

---
+++ Classes/Utility/MathematicUtility.php
@@ -0,0 +1,35 @@
+<?php
+namespace In2code\Powermail\Utility;
+
+/**
+ * Class MathematicUtility
+ */
+class MathematicUtility
+{
+    /**
+     * Basic mathematic operation with two variables and one operator
+     *
+     * @param int $number1
+     * @param int $number2
+     * @param string $operator +|-|x|:
+     * @return int
+     */
+    public static function mathematicOperation($number1, $number2, $operator = '+')
+    {
+        switch ($operator) {
+            case '-':
+                $result = $number1 - $number2;
+                break;
+            case 'x':
+                $result = $number1 * $number2;
+                break;
+            case ':':
+                $result = $number1 / $number2;
+                break;
+            case '+':
+            default:
+                $result = $number1 + $number2;
+        }
+        return $result;
+    }
+}
