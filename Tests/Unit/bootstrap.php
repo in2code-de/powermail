1516d9d85b20047ee7e0d52ac0d8d2ba7f4c4b27
Alex Kellner <alexander.kellner@in2code.de>
2017-12-25 14:19:31 +0100

    [TASK] Add a bootstrap.php for unit tests which is compatible with T3 7.6 and 8.7

---
+++ Tests/Unit/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+if (empty($webRoot = getenv('TYPO3_PATH_WEB'))) {
+    putenv('TYPO3_PATH_WEB=' . $webRoot = realpath(__DIR__ . '/../../.Build/Web') . '/');
+} else {
+    $webRoot = rtrim($webRoot, '/') . '/';
+}
+$buildRoot = realpath($webRoot . '/..');
+$autoload = $buildRoot . '/vendor/autoload.php';
+if (!file_exists($autoload)) {
+    throw new \LogicException('Can not find autoload path');
+}
+
+$bootstrapLoaded = false;
+$bootstrap = $webRoot . '/typo3/sysext/core/Build/UnitTestsBootstrap.php';
+$bootstrapNimut = $buildRoot . '/vendor/nimut/testing-framework/res/Configuration/UnitTestsBootstrap.php';
+if (file_exists($bootstrap)) {
+    require($bootstrap);
+    $bootstrapLoaded = true;
+} elseif (file_exists($bootstrapNimut)) {
+    require($bootstrapNimut);
+    $bootstrapLoaded = true;
+}
+if ($bootstrapLoaded === false) {
+    throw new \LogicException('Can not find unit test bootstrap file. Did you do a composer update?');
+}
