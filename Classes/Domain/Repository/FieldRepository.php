307a32da4a003eafd6cc236c635e93530ca27894
Alex Kellner <alexander.kellner@in2code.de>
2018-01-10 21:39:15 +0100

    [TASK] Replace TYPO3_db call with querybuilder in FieldRepository

--- Classes/Domain/Repository/FieldRepository.php
+++ Classes/Domain/Repository/FieldRepository.php
@@ -7,35 +7,8 @@ use In2code\Powermail\Domain\Model\Page;
 use In2code\Powermail\Utility\ConfigurationUtility;
 use In2code\Powermail\Utility\DatabaseUtility;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2012 in2code GmbH <info@in2code.de>
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 /**
- * FieldRepository
- *
- * @package powermail
- * @license http://www.gnu.org/licenses/lgpl.html
- *          GNU Lesser General Public License, version 3 or later
+ * Class FieldRepository
  */
 class FieldRepository extends AbstractRepository
 {
@@ -122,14 +95,15 @@ class FieldRepository extends AbstractRepository
     public function findAllWrongLocalizedFields()
     {
         $pages = [];
-        $select = 'uid,pid,title,l10n_parent,sys_language_uid';
-        $from = Field::TABLE_NAME;
-        $where = '(pages = "" or pages = 0) and sys_language_uid > 0 and deleted = 0';
-        $res = $this->getDatabaseConnection()->exec_SELECTquery($select, $from, $where);
-        if ($res) {
-            while (($row = $this->getDatabaseConnection()->sql_fetch_assoc($res))) {
-                $pages[] = $row;
-            }
+        $queryBuilder = DatabaseUtility::getQueryBuilderForTable(Field::TABLE_NAME, true);
+        $rows = $queryBuilder
+            ->select('uid', 'pid', 'title', 'l10n_parent', 'sys_language_uid')
+            ->from(Field::TABLE_NAME)
+            ->where('(pages = "" or pages = 0) and sys_language_uid > 0 and deleted = 0')
+            ->execute()
+            ->fetchAll();
+        foreach ($rows as $row) {
+            $pages[] = $row;
         }
         return $pages;
     }
