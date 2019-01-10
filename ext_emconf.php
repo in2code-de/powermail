0fc3db1b3b44d7ac5dd88fc701ab08c5518fda80
Alex Kellner <alexander.kellner@in2code.de>
2019-01-10 14:15:18 +0100

    [TASK] Remove deprecated keys in ext_emconf.php
    Related: https://github.com/einpraegsam/powermail/issues/360

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,6 @@
 <?php
-
 /***************************************************************
  * Extension Manager/Repository config file for ext "powermail".
- *
- * Auto generated 04-07-2013 17:03
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
@@ -16,24 +9,15 @@ $EM_CONF[$_EXTKEY] = [
         and easy to use mailform extension with a lots of features
         (spam prevention, marketing information, optin, ajax submit, diagram analysis, etc...)',
     'category' => 'plugin',
-    'shy' => 0,
     'version' => '7.0.0',
-    'dependencies' => 'cms,extbase,fluid',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
     'module' => '',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => 'uploads/tx_powermail,typo3temp/tx_powermail',
-    'modify_tables' => '',
     'clearcacheonload' => 1,
-    'lockType' => '',
     'author' => 'Powermail Development Team',
     'author_email' => 'alexander.kellner@in2code.de',
     'author_company' => 'in2code.de',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
