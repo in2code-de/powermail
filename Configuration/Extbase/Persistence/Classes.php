82aa5240048988e35f03ef0bda2db467f1ab82cb
Gerald Jelitto <gerald.jelitto@googlemail.com>
2020-04-29 17:54:06 +0200

    Create Classes.php
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    \In2code\Powermail\Domain\Model\User::class => [
+        'tableName' => 'fe_users'
+    ],
+    \In2code\Powermail\Domain\Model\UserGroup::class => [
+        'tableName' => 'fe_groups'
+    ]
+];
