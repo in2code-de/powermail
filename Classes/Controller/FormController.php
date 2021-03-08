in2code-de/powermail

commit 95084339151e6b47ac45cc951b02586286bea316

[BUGFIX] Fix possible exception with additional validators or spam prevention methods
If someone extends powermail with own validators and is using "show confirmation page" it could happen that the error
'Exception while property mapping at property path "": It is not allowed to map property "answers". You need to use $propertyMappingConfiguration->allowProperties('answers') to enable mapping of this property.'
pops up. Problem is basicly because createAction with its validators forwarded the request to confirmationAction with the same validators

Classes/Controller/FormController.php
@@ -434,6 +434,22 @@ class FormController extends AbstractController
     }
 
     /**
+     * Always forward to formAction if a validation fails. Otherwise it could happen that when
+     * a validator for createAction fails, confirmationAction is called (if function is turned on) and same validators
+     * are firing again
+     *
+     * @return void
+     * @throws StopActionException
+     */
+    protected function forwardToReferringRequest()
+    {
+        $originalRequest = clone $this->request;
+        $this->request->setOriginalRequest($originalRequest);
+        $this->request->setOriginalRequestMappingResults($this->arguments->validate());
+        $this->forward('form');
+    }
+
+    /**
      * Decide if the mail object should be persisted or not
      *        persist if
      *            - enabled with TypoScript AND hash is not set OR

commit 0dc7c81951995f677030f0719c1054fbd6466ebb

[BUGFIX] Fix regression introduced with 95084339151e6b47ac45cc951b02586286bea316
In TYPO3 8 the method $this->arguments->validate() had the name getValidationResults(). So we have to use this method name as long as TYPO3 8 and 9 is supported in this branch

Classes/Controller/FormController.php
@@ -445,7 +445,12 @@ class FormController extends AbstractController
     {
         $originalRequest = clone $this->request;
         $this->request->setOriginalRequest($originalRequest);
-        $this->request->setOriginalRequestMappingResults($this->arguments->validate());
+        if (\In2code\Powermail\Utility\ConfigurationUtility::isTypo3OlderThen9()) {
+            $validationResult = $this->arguments->getValidationResults();
+        } else {
+            $validationResult = $this->arguments->validate();
+        }
+        $this->request->setOriginalRequestMappingResults($validationResult);
         $this->forward('form');
     }
 