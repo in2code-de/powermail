c8e49ceadc47b2b05e50239d8258fb0354c35025
Marcus Schwemer <marcus.schwemer@in2code.de>
2022-01-07 19:42:08 +0100

    [CLEANUP] Remove unused use statement
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

--- Classes/Eid/GetLocationEid.php
+++ Classes/Eid/GetLocationEid.php
@@ -2,7 +2,6 @@
 declare(strict_types = 1);
 namespace In2code\Powermail\Eid;
 
-use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Exception;
