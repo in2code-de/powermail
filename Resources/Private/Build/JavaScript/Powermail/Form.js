ebe30b85b167d5ee708a837f3969edcf382c49e7
Alexander Kellner <alexander.kellner@in2code.de>
2022-04-06 11:33:48 +0200

    [FEATURE] Remove jQuery: Fill location fields without jQuery

--- Resources/Private/Build/JavaScript/Powermail/Form.js
+++ Resources/Private/Build/JavaScript/Powermail/Form.js
@@ -5,8 +5,9 @@ function PowermailForm() {
   'use strict';
 
   this.initialize = function () {
-    formValidationListener();
+    // formValidationListener();
     moreStepFormListener();
+    locationFieldListener();
   };
 
   let formValidationListener = function() {
@@ -24,6 +25,29 @@ function PowermailForm() {
   let moreStepFormListener = function() {
     let moreStepForm = new MoreStepForm();
     moreStepForm.initialize();
+  };
+
+  let locationFieldListener = function() {
+    if (document.querySelector('[data-powermail-location="prefill"]') !== null) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        let base = document.querySelector('[data-powermail-eidurl]').getAttribute('data-powermail-eidurl');
+        let url = base + '?eID=' + 'powermailEidGetLocation&lat=' + lat + '&lng=' + lng;
+
+        fetch(url)
+          .then((resp) => resp.text())
+          .then(function(data) {
+            let elements = document.querySelectorAll('[data-powermail-location="prefill"]');
+            for (let i = 0; i < elements.length; i++) {
+              elements[i].value = data;
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      });
+    }
   };
 }
 
