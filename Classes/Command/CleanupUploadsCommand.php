e1023f201327bc9075728edca964ab4edf8aec8c
Marcus Schwemer <marcus.schwemer@in2code.de>
2022-04-14 12:06:16 +0200

    [BUGFIX] Add namespace declaration for command
    The namespace declaration for command was missing, leading to an undefined
    constant. This is fixed now.
    
    Resolves: #746
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

--- Classes/Command/CleanupUploadsCommand.php
+++ Classes/Command/CleanupUploadsCommand.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 namespace In2code\Powermail\Command;
 
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
