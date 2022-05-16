6106c70cc6f4b8e00f47df98ab7ee882a004eab3
nebrot <nebrot@users.noreply.github.com>
2022-05-16 11:05:06 +0200

    [FEATURE] Add min and max validation for date fields
    Fixes #765

--- Resources/Private/Build/JavaScript/FormValidation.js
+++ Resources/Private/Build/JavaScript/FormValidation.js
@@ -1,4 +1,5 @@
 import Utility from './Utility';
+import moment from 'moment';
 
 export default class FormValidation {
   #formValidationSelector = '[data-powermail-validate]';
@@ -321,7 +322,12 @@ class Form {
       return true;
     }
     let minimum = field.getAttribute('min') || field.getAttribute('data-powermail-min');
-    return parseInt(field.value) >= parseInt(minimum);
+    let value = field.value;
+    if (field.getAttribute('type') === 'date' || field.getAttribute('type') === 'datetime-local' || field.getAttribute('type') === 'time') {
+      value = this.#getUnixTimestamp(value, field.getAttribute('data-datepicker-format'));
+      minimum = this.#getUnixTimestamp(minimum, field.getAttribute('data-datepicker-format'));
+    }
+    return parseInt(value) >= parseInt(minimum);
   };
 
   #isValidationMaximumConfirmed(field) {
@@ -329,7 +335,12 @@ class Form {
       return true;
     }
     let maximum = field.getAttribute('max') || field.getAttribute('data-powermail-max');
-    return parseInt(field.value) <= parseInt(maximum);
+    let value = field.value;
+    if (field.getAttribute('type') === 'date' || field.getAttribute('type') === 'datetime-local' || field.getAttribute('type') === 'time') {
+      value = this.#getUnixTimestamp(value, field.getAttribute('data-datepicker-format'));
+      maximum = this.#getUnixTimestamp(maximum, field.getAttribute('data-datepicker-format'));
+    }
+    return parseInt(value) <= parseInt(maximum);
   };
 
   #isValidationLengthConfirmed(field) {
@@ -452,6 +463,14 @@ class Form {
       if (selectedField !== null) {
         value = selectedField.value;
       }
+    }
+    return value;
+  };
+  
+  #getUnixTimestamp(value, formatInput) {
+    let momentDate = moment(value, formatInput);
+    if (momentDate.isValid) {
+      value = momentDate.unix();
     }
     return value;
   };
