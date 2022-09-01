62b40690231aee4f7af10e3b0e9690a258c7666d
Alexander Kellner <alexander.kellner@in2code.de>
2022-09-01 10:54:13 +0200

    [TASK] Make CS fixer happy

--- Classes/Utility/ConfigurationUtility.php
+++ Classes/Utility/ConfigurationUtility.php
@@ -7,8 +7,8 @@ use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotCon
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Core\Environment;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\ArrayUtility as CoreArrayUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class ConfigurationUtility
