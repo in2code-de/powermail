7c90728e1cbce0bb479cba2fb1a56960cb9fc18b
Marcus Schwemer <marcus.schwemer@in2code.de>
2022-12-16 14:54:09 +0100

    [TASK] Move icon registry to service container
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'extension-powermail-main' => [
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:powermail/Resources/Public/Icons/powermail.svg'
+    ]
+];
