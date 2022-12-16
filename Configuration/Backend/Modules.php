78d8648369a6554a5e0873fafa21977b7dcd5e03
Marcus Schwemer <marcus.schwemer@in2code.de>
2022-12-16 14:56:46 +0100

    [TASK] Move module registration to service container
    See also: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-96733-NewBackendModuleRegistrationAPI.html
    
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'web_powermail' => [
+        'parent' => 'web',
+        'position' => [],
+        'access' => 'user, group',
+        'iconIdentifier' => 'extension-powermail-main',
+        'labels' => 'LLL:EXT:powermail/Resources/Private/Language/locallang_mod.xlf',
+        'path' => '/module/web/Powermail',
+        'extensionname' => 'Powermail',
+        'controllerActions' => [
+            \In2code\Powermail\Controller\ModuleController::class =>
+                'dispatch, list, exportXls, exportCsv, reportingBe, toolsBe, overviewBe, ' .
+                'checkBe, converterBe, converterUpdateBe, reportingFormBe, reportingMarketingBe, ' .
+                'fixUploadFolder, fixWrongLocalizedForms, fixFilledMarkersInLocalizedFields, ' .
+                'fixWrongLocalizedPages, fixFilledMarkersInLocalizedPages'
+        ]
+    ],
+];
