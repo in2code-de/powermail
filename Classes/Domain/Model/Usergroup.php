96aff391503617ca1db3b286c5bbf249f886b7fc
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-03-14 11:11:47 +0100

    Revert "[TASK] Add extbase user and usergroup model"
    This reverts commit 06a371192b7fd290377fdd7a3f1ae78645082070.

--- Classes/Domain/Model/Usergroup.php
+++ Classes/Domain/Model/Usergroup.php
@@ -1,72 +1,13 @@
 <?php
 
 declare(strict_types=1);
-
 namespace In2code\Powermail\Domain\Model;
 
-use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
-use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+use TYPO3\CMS\Extbase\Domain\Model\FrontendUserGroup;
 
-class Usergroup extends AbstractEntity
+/**
+ * Class UserGroup
+ */
+class UserGroup extends FrontendUserGroup
 {
-    const TABLE_NAME = 'fe_groups';
-
-    protected string $title = '';
-    protected string $description = '';
-
-    /**
-     * @var ObjectStorage<Usergroup>
-     */
-    protected ObjectStorage $subgroup;
-
-    public function __construct(string $title = '')
-    {
-        $this->setTitle($title);
-        $this->subgroup = new ObjectStorage();
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-        return $this;
-    }
-
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-        return $this;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function setSubgroup(ObjectStorage $subgroup): self
-    {
-        $this->subgroup = $subgroup;
-        return $this;
-    }
-
-    public function addSubgroup(Usergroup $subgroup): self
-    {
-        $this->subgroup->attach($subgroup);
-        return $this;
-    }
-
-    public function removeSubgroup(Usergroup $subgroup): self
-    {
-        $this->subgroup->detach($subgroup);
-        return $this;
-    }
-
-    public function getSubgroup(): ObjectStorage
-    {
-        return $this->subgroup;
-    }
 }
