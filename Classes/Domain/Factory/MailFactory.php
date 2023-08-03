71aecb4778fb6e8d7fb2ee1ac9b524a8bd05c124
Ayke Halder <rr-it@users.noreply.github.com>
2023-08-03 11:51:08 +0200

    Persist mail without css in body

--- Classes/Domain/Factory/MailFactory.php
+++ Classes/Domain/Factory/MailFactory.php
@@ -11,11 +11,11 @@ use In2code\Powermail\Utility\FrontendUtility;
 use In2code\Powermail\Utility\SessionUtility;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
-use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\Exception;
 use TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotException;
 use TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotReturnException;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * Class MailFactory
@@ -42,7 +42,7 @@ class MailFactory
             ->setSenderName($mailRepository->getSenderNameFromArguments($mail))
             ->setSubject($settings['receiver']['subject'])
             ->setReceiverMail($settings['receiver']['email'])
-            ->setBody(DebugUtility::viewArray($mailRepository->getVariablesWithMarkersFromMail($mail)))
+            ->setBody(DebuggerUtility::var_dump($mailRepository->getVariablesWithMarkersFromMail($mail), '', 8, true, false, true))
             ->setSpamFactor(SessionUtility::getSpamFactorFromSession())
             ->setTime((time() - SessionUtility::getFormStartFromSession($mail->getForm()->getUid(), $settings)))
             ->setUserAgent(GeneralUtility::getIndpEnv('HTTP_USER_AGENT'))
