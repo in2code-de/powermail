3dc455eed84a19916f91770179500e6797227065
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-09-18 16:35:21 +0200

    [TASK] Release preparations for 11.1.0
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
         and easy to use mailform extension with a lots of features
         (spam prevention, marketing information, optin, ajax submit, diagram analysis, etc...)',
     'category' => 'plugin',
-    'version' => '11.0.0',
+    'version' => '11.1.0',
     'state' => 'stable',
     'author' => 'Powermail Development Team',
     'author_email' => 'service@in2code.de',
