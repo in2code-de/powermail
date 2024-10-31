8a006a19f0272b66b7b7059edcc9df28df87bed4
Andreas Nedbal <andreas.nedbal@in2code.de>
2024-10-31 17:07:34 +0100

    [TASK] Adjust build setup to build a Backend ES module
    Related: https://projekte.in2code.de/issues/67717

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dependencies' => ['core', 'backend'],
+    'imports' => [
+        '@in2code/powermail/backend' => 'EXT:powermail/Resources/Public/JavaScript/Powermail/Backend.min.js',
+    ],
+];
--- Resources/Private/Build/rollup.config.js
+++ Resources/Private/Build/rollup.config.js
@@ -4,22 +4,47 @@ const commonjs = require('@rollup/plugin-commonjs');
 const { terser } = require('rollup-plugin-terser');
 
 module.exports = {
-  input: './JavaScript/Form.js',
-  plugins: [
-    resolve({
-      browser: true
-    }),
-    commonjs({
-      sourceMap: false
-    }),
-    babel({
-      exclude: './node_modules/**',
-      babelHelpers: 'bundled'
-    }),
-    terser()
-  ],
-  output: {
-    file: '../../Public/JavaScript/Powermail/Form.min.js',
-    format: 'iife'
+  form: {
+    input: './JavaScript/Form.js',
+    plugins: [
+      resolve({
+        browser: true
+      }),
+      commonjs({
+        sourceMap: false
+      }),
+      babel({
+        exclude: './node_modules/**',
+        babelHelpers: 'bundled'
+      }),
+      terser()
+    ],
+    output: {
+      file: '../../Public/JavaScript/Powermail/Form.min.js',
+      format: 'iife'
+    },
   },
+  backend: {
+    external: [
+        /^@typo3\//,
+        'jquery'
+    ],
+    input: './JavaScript/Backend.js',
+    plugins: [
+      resolve({
+        browser: true
+      }),
+      commonjs({
+        sourceMap: false
+      }),
+      babel({
+        exclude: './node_modules/**',
+        babelHelpers: 'bundled'
+      })
+    ],
+    output: {
+      file: '../../Public/JavaScript/Powermail/Backend.min.js',
+      format: 'esm'
+    },
+  }
 };
