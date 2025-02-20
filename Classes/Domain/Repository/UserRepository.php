24b742d5a4c202378af83bad032d0ba4438c74b3
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-02-20 12:17:34 +0100

    [TASK] Make phpstan happy

--- Classes/Domain/Repository/UserRepository.php
+++ Classes/Domain/Repository/UserRepository.php
@@ -28,7 +28,7 @@ class UserRepository extends AbstractRepository
      * Find by Uid but don't respect storage page
      *
      * @param int $uid
-     * @return User
+     * @return User|null
      */
     public function findByUid($uid): ?User
     {
@@ -36,7 +36,8 @@ class UserRepository extends AbstractRepository
         $query->getQuerySettings()->setRespectStoragePage(false);
         $query->matching($query->equals('uid', $uid));
         /** @var User|null $user */
-        return $query->execute()->getFirst();
+        $user = $query->execute()->getFirst();
+        return $user;
     }
 
     /**
