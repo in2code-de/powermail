2ef6154c9a58492837efcb56256379581f2d7ad1
Mohsin Khan <mohsinqayyumkhan@gmail.com>
2025-05-23 16:49:54 +0500

    aria-required not necessary when native required present

--- Tests/Unit/ViewHelpers/Validation/DatepickerDataAttributeViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Validation/DatepickerDataAttributeViewHelperTest.php
@@ -64,7 +64,6 @@ class DatepickerDataAttributeViewHelperTest extends UnitTestCase
                     'data-datepicker-format' => 'YYYY-MM-DD HH:mm',
                     'data-date-value' => 'anyvalue',
                     'required' => 'required',
-                    'aria-required' => 'true',
                     'data-powermail-required-message' => 'validationerror_mandatory',
                 ],
             ],
