99b2364cc6acd1ea82fc88a05ee5bc4bcbaf293c
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-07-11 12:46:09 +0200

    [TASK] Adjust stability information in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
         (spam prevention, marketing information, optin, ajax submit, diagram analysis, etc...)',
     'category' => 'plugin',
     'version' => '13.0.0',
-    'state' => 'beta',
+    'state' => 'stable',
     'author' => 'Powermail Development Team',
     'author_email' => 'service@in2code.de',
     'author_company' => 'in2code.de',
