66e46a0047d56d5dd70cf0f50a4c43edf81e1376
Marcus Schwemer <marcus.schwemer@in2code.de>
2026-02-25 10:35:43 +0100

    [BUGFIX] Small fixes for autocomplete functionality
    Related: in2code-de/powermail#1330

--- Configuration/TCA/tx_powermail_domain_model_field.php
+++ Configuration/TCA/tx_powermail_domain_model_field.php
@@ -1067,7 +1067,7 @@ $fieldsTca = [
             ],
             'displayCond' => [
                 'AND' => [
-                    'FIELD:type:IN:input,textarea,password,select,country,location,hidden',
+                    'FIELD:type:IN:input,textarea,select,country,location,hidden',
                     'FIELD:autocomplete_token:!IN:on,off,nickname,sex,impp,url,organization-title,username,new-password,current-password,one-time-code,bday,bday-day,bday-month,bday-year,language,photo',
                     'FIELD:autocomplete_token:REQ:true',
                 ],
@@ -1144,7 +1144,7 @@ $fieldsTca = [
                     'allowLanguageSynchronization' => true,
                 ],
             ],
-            'displayCond' => 'FIELD:type:IN:' . $fieldsWithAutocompleteOptions,
+            'displayCond' => 'FIELD:type:IN:input,textarea,select,country,location,hidden',
         ],
     ],
 ];
