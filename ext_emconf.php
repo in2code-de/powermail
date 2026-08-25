a04db6e18d090f9edb7409201933a76e29f126d8
Shentao <taonguyen@gmx.net>
2026-08-25 15:02:00 +0200

    Update powermail extension configuration
    Updated version from 7.5.1 to 7.5.2 and changed uploadfolder and clearcacheonload settings.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,31 +1,42 @@
 <?php
+
 /***************************************************************
  * Extension Manager/Repository config file for ext "powermail".
+ *
+ * Auto generated 12-02-2025 15:17
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'powermail',
-    'description' => 'Powermail is a well-known, editor-friendly, powerful
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'powermail',
+  'description' => 'Powermail is a well-known, editor-friendly, powerful
         and easy to use mailform extension with a lots of features
         (spam prevention, marketing information, optin, ajax submit, diagram analysis, etc...)',
-    'category' => 'plugin',
-    'version' => '7.5.1',
-    'module' => '',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => 'uploads/tx_powermail,typo3temp/assets/tx_powermail',
-    'clearcacheonload' => 1,
-    'author' => 'Powermail Development Team',
-    'author_email' => 'alexander.kellner@in2code.de',
-    'author_company' => 'in2code.de',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'php' => '7.0.0-7.99.99'
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [],
-    ],
-    '_md5_values_when_last_written' => '',
-];
+  'category' => 'plugin',
+  'version' => '7.5.2',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'createDirs' => 'uploads/tx_powermail,typo3temp/assets/tx_powermail',
+  'clearcacheonload' => false,
+  'author' => 'Powermail Development Team',
+  'author_email' => 'alexander.kellner@in2code.de',
+  'author_company' => 'in2code.de',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.0-9.5.99',
+      'php' => '7.0.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => '',
+);
