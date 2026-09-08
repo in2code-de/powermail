071b1fe8114c55bacef7dec74625c257f209becd
Marcus Schwemer <marcus.schwemer@in2code.de>
2026-09-08 17:43:43 +0200

    [TASK] Provide rudimentary ddev setup for testing

---
+++ .project/typo3/AdditionalConfiguration.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * #ddev-generated: Automatically generated TYPO3 additional.php file.
+ * ddev manages this file and may delete or overwrite the file unless this comment is removed.
+ * It is recommended that you leave this file alone.
+ */
+
+if (getenv('IS_DDEV_PROJECT') == 'true') {
+    $ddevConfig = [
+        // This GFX configuration allows processing by installed ImageMagick 6
+        'GFX' => [
+            'processor' => 'ImageMagick',
+            'processor_path' => '/usr/bin/',
+            'processor_path_lzw' => '/usr/bin/',
+        ],
+        // This mail configuration sends all emails to mailpit
+        'MAIL' => [
+            'transport' => 'smtp',
+            'transport_smtp_encrypt' => false,
+            'transport_smtp_server' => 'localhost:1025',
+        ],
+        'SYS' => [
+            'trustedHostsPattern' => '.*.*',
+            'devIPmask' => '*',
+            'displayErrors' => 1,
+        ],
+    ];
+
+    // Only override the database connection if the project uses a driver
+    // provided by the DDEV db container. If the Default connection has been
+    // configured for anything else (like SQLite), leave it alone.
+    $ddevDriver = $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] ?? 'mysqli';
+    if (in_array($ddevDriver, ['mysqli', 'pdo_mysql', 'pdo_pgsql'], true)) {
+        $ddevConfig['DB'] = [
+            'Connections' => [
+                'Default' => [
+                    'dbname' => 'db',
+                    'driver' => 'mysqli',
+                    'host' => 'db',
+                    'password' => 'db',
+                    'port' => 3306,
+                    'user' => 'db',
+                ],
+            ],
+        ];
+    }
+
+    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+        $GLOBALS['TYPO3_CONF_VARS'],
+        $ddevConfig
+    );
+    unset($ddevConfig, $ddevDriver);
+}
