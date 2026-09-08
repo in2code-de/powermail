2004a548cd9f809c0de204f0e12ff04e01501a82
Marcus Schwemer <marcus.schwemer@in2code.de>
2026-09-08 10:52:04 +0200

    [BUGFIX] Respect pagination setting in ModuleController
    It is possible to set the maximum number of items for the
    pagination in the ModuleController. This value was not respected,
    while initializing the pagination.
    
    Related: in2code-de/powermail#1404
    (cherry picked from commit e5fe598a6cdfe6c9c418658b3d373ab1101bf86e)

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -116,7 +116,7 @@ class ModuleController extends AbstractController
 
         $itemsPerPage = (int)($this->settings['perPage'] ?? 10);
         $paginator = GeneralUtility::makeInstance(QueryResultPaginator::class, $mails, $currentPage, $itemsPerPage);
-        $pagination = GeneralUtility::makeInstance(SlidingWindowPagination::class, $paginator, 15);
+        $pagination = GeneralUtility::makeInstance(SlidingWindowPagination::class, $paginator, $itemsPerPage);
 
         $firstFormUid = StringUtility::conditionalVariable($this->piVars['filter']['form'] ?? '', key($formUids));
         $beUser = BackendUtility::getBackendUserAuthentication();
@@ -132,7 +132,7 @@ class ModuleController extends AbstractController
                 'paginator' => $paginator,
             ],
             'settings' => $this->settings,
-            'perPage' => $this->settings['perPage'] ?? 10,
+            'perPage' => $itemsPerPage,
             'writeAccess' => $beUser->check('tables_modify', Answer::TABLE_NAME)
                 && $beUser->check('tables_modify', Mail::TABLE_NAME),
             'activateXlsxExport' => $this->isPhpSpreadsheetInstalled,
